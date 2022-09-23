977bc62c2fdf1c06aa670c72420224343702769d
woohyuk <dngur6344@gmail.com>
2022-09-23 18:34:05 +0900

    api gateway에서는 DB 접속 비활성.

--- src/main/java/com/project/apigateway/security/ResourceServerConfig.java
+++ src/main/java/com/project/apigateway/security/ResourceServerConfig.java
@@ -1,28 +1,28 @@
-package com.project.apigateway.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    JwtTokenProvider jwtTokenProvider;
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated()
-            .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-    }
-}
+//package com.project.apigateway.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    JwtTokenProvider jwtTokenProvider;
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception{
+//        http.csrf().disable().authorizeRequests()
+//                .antMatchers("/auth/**").permitAll()
+//                .anyRequest().authenticated()
+//            .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+//    }
+//}
--- src/main/java/com/project/apigateway/user/UserInformationService.java
+++ src/main/java/com/project/apigateway/user/UserInformationService.java
@@ -1,55 +1,55 @@
-package com.project.apigateway.user;
-
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-
-@Service("UserInformationService")
-public class UserInformationService implements UserDetailsService {
-
-    @Resource(name="UserDao")
-    private UserDao UserDao;
-
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        User user = UserDao.findByUsername(username);
-
-        // 저장된 ID가 없을때 throw 시켜줍니다.
-        if(user == null) {
-            throw new UsernameNotFoundException("wrongId"); // 저장된 ID 없음
-        }
-        return makeLoginUser(user);
-    }
-
-    // UserInformation 값 주입 해 줍니다.
-    public UserInformation makeLoginUser(User user) {
-
-        UserInformation loginUser  = new UserInformation();
-
-        /*List<GrantedAuthority> Authoritylist = new ArrayList<>();
-        switch(user.getUserType()) {
-            case 0 :
-                // admin
-                Authoritylist.add(new SimpleGrantedAuthority("ADMIN"));
-            case 1 :
-                // user
-                Authoritylist.add(new SimpleGrantedAuthority("USER"));
-                break;
-        }*/
-
-        loginUser.setUsername(user.getUsername());
-        loginUser.setPassword(user.getPassword());
-        //loginUser.setAuthorities(Authoritylist);
-        loginUser.setKoreanname(user.getKoreanname());
-        loginUser.setBirthday(user.getBirthday());
-
-        System.out.println(user.getUsername());
-        return loginUser;
-    }
-}
+//package com.project.apigateway.user;
+//
+//
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import javax.annotation.Resource;
+//
+//@Service("UserInformationService")
+//public class UserInformationService implements UserDetailsService {
+//
+//    @Resource(name="UserDao")
+//    private UserDao UserDao;
+//
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        User user = UserDao.findByUsername(username);
+//
+//        // 저장된 ID가 없을때 throw 시켜줍니다.
+//        if(user == null) {
+//            throw new UsernameNotFoundException("wrongId"); // 저장된 ID 없음
+//        }
+//        return makeLoginUser(user);
+//    }
+//
+//    // UserInformation 값 주입 해 줍니다.
+//    public UserInformation makeLoginUser(User user) {
+//
+//        UserInformation loginUser  = new UserInformation();
+//
+//        /*List<GrantedAuthority> Authoritylist = new ArrayList<>();
+//        switch(user.getUserType()) {
+//            case 0 :
+//                // admin
+//                Authoritylist.add(new SimpleGrantedAuthority("ADMIN"));
+//            case 1 :
+//                // user
+//                Authoritylist.add(new SimpleGrantedAuthority("USER"));
+//                break;
+//        }*/
+//
+//        loginUser.setUsername(user.getUsername());
+//        loginUser.setPassword(user.getPassword());
+//        //loginUser.setAuthorities(Authoritylist);
+//        loginUser.setKoreanname(user.getKoreanname());
+//        loginUser.setBirthday(user.getBirthday());
+//
+//        System.out.println(user.getUsername());
+//        return loginUser;
+//    }
+//}
